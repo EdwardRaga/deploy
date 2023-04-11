65ee1e8cd1718712b6172fc3dbba6ae196b8963e
Edward Raga <edwardraga@icloud.com>
2023-04-11 19:53:50 -0400

    footer css

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import Form from './components/Form/Form';
 import Detail from './components/Detail/Detail';
 import Landing from './components/Landing/Landing';
 import { Route, useLocation} from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 import axios from "axios";
 axios.defaults.baseURL = 'https://deploy-production-81f7.up.railway.app/';
-
 
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
      <Route path={"/home"} component={Home}/>
      <Route exact path={"/addgame"} component={Form}/>
      <Route exact path={"/detail/:id"} component={Detail}/>
+     {location.pathname !== '/' && <Footer/>}
      
      
     </div>
